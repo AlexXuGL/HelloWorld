2e1904618e6844f24c62a2f6f7aa4721defbe5b5
EricXu <AlexXu@dhcp-10-105-134-89.wireless.northwestern.private>
2017-09-08 14:52:06 -0500

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created by AlexXu on 18/03/2017.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println();
+    }
+
+    public Hello() {
+    }
+}
